bf86226af1a0200aa32785afa0b1b9d12198cfa3
minalkashif1 <minal.akashif1@gmail.com>
2025-08-01 13:57:00 +0500

    Initial commit to feature branch

---
+++ to-do-list/src/main/java/com/example/demo/Model/User.java
@@ -0,0 +1,80 @@
+package com.example.demo.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Entity
+@Table(name = "[user]")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "User_ID")
+    private Long id;
+
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String username;
+    private String email;
+    private int velocity;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Task> tasks;
+
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public int getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+
+
+}
